f8666ecf1852684d9a7374becce5c4f7903f8e4b
rohitagr0310 <rohitagr2610@gmail.com>
2024-08-22 09:53:40 +0530

    index updated

--- Day09 - prompt_sync/index.js
+++ Day09 - prompt_sync/index.js
@@ -1,14 +1,9 @@
-// Import the prompt-sync module
 const prompt = require("prompt-sync")();
 
-// Prompt the user to enter a number
-const userInput = prompt("Enter a number: ");
+const input = prompt("Enter a number: ");
 
-// Convert the input to a number
-const number = parseFloat(userInput);
+const number = parseFloat(input);
 
-// Check if the number is positive, negative, or zero
-console.log(number);
 if (number > 0) {
   console.log("The number is positive");
 } else if (number < 0) {
